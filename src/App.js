c6b6f69a859b5b8eeb179c0e8d7fe18dfa8573db
sumona8436 <sumonasonai50@gmai.com>
2024-01-14 20:54:38 +0530

    TodoListBasic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import { useState } from "react";
-// let status{
-//   c: 0 //it will effect only first time not for second time
-// }
 
 function App() {
-  const [c, setC] = useState(0);
-  function clickHandlerFunction() {
-    // status.c=status.c+1;  //if you do like this to increse the c number,
-    //console.log( status.c);  //then you can see in log,state is increasing by 1,but not redendaring
-    setC(c + 1);
+  const [todoState, settodoState] = useState([
+    {
+      name: "Gym",
+      description: "You have to go gym",
+    },
+    {
+      name: "Breakfast",
+      description: "You have to take breakfast",
+    },
+  ]);
+
+  //if you click  button,it will add one new todo with existed todo
+  function addingNew() {
+    let newTodo = [
+      ...todoState,
+      {
+        name: "Study",
+        description: "You have to Study",
+      },
+    ];
+    settodoState(newTodo);
   }
+
   return (
     <>
       <button
-        style={{ textAlign: "center", marginleft: "20px" }}
-        onClick={clickHandlerFunction}
+        style={{
+          color: "red",
+          textAlign: "center",
+          marginLeft: "570px",
+          marginTop: "20px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onClick={addingNew}
       >
-        count {c}
+        Add new Todo
       </button>
+      {/* redendaring */}
+
+      {/* {JSON.stringify(todoState)}  //object array -> string */}
+
+      {/* <TodoComponents
+        name={todoState[0].name}
+        description={todoState[0].description}
+      />
+      <TodoComponents
+        name={todoState[1].name}
+        description={todoState[1].description}
+      /> */}
+      {/* iterate over state */}
+      {todoState.map(function (param) {
+        return (
+          <TodoComponents name={param.name} description={param.description} />
+        );
+      })}
     </>
   );
 }
-
+function TodoComponents(prop) {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ color: "greenyellow" }}>{prop.name}</h1>
+      <h2>{prop.description}</h2>
+    </div>
+  );
+}
 export default App;
